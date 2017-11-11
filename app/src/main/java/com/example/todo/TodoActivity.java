20a13c823f8f0a10fa5c62e42383229c980e20e3
MelGitAcc <emgeexy@yahoo.com>
2017-11-11 21:30:38 +0000

    Activity and Intent Done

--- app/src/main/java/com/example/todo/TodoActivity.java
+++ app/src/main/java/com/example/todo/TodoActivity.java
@@ -2,17 +2,39 @@ package com.example.todo;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TodoActivity extends AppCompatActivity {
 
     private String[] mTodos;
     private int mTodoIndex = 0;
+
+    private static final int IS_SUCCESS = 0;//initialising int variable use for detail button
+    public static final String TAG = "TodoActivity";
+
+    // name, value pair to be returned in an intent
+    private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
+
+    // ----------------In case of state change, due to rotating the phone-----------------
+    // store the mTodoIndex to display the same mTodos element post state change
+    // N.B. small amounts of data, typically IDs can be stored as key, value pairs in a Bundle
+    // the alternative is to abstract view data to a ViewModel which can be in scope in all
+    // Activity states and more suitable for larger amounts of data
+    private static final String TODO_INDEX = "com.example.todoIndex";
+    // override to write the value of mTodoIndex into the Bundle with TODO_INDEX as its key
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +43,23 @@ public class TodoActivity extends AppCompatActivity {
         // the view hierarchy
         super.onCreate(savedInstanceState);
 
+        Log.d(TAG, " PC is in onCreate!");
+
         // set the user interface layout for this Activity
         // the layout file is defined in the project res/layout/activity_todo.xml file
         setContentView(R.layout.activity_todo);
 
+        // ---------check for saved state due to changes such as rotation or back button
+        // and restore any saved state such as the todo index
+        if (savedInstanceState != null){
+            mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
+        }
+
         // initialize member TextView so we can manipulate it later
         final TextView TodoTextView;
         TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+
+        setTextViewComplete("");
 
         // read the todo array from res/values/strings.xml
         Resources res = getResources();
@@ -67,5 +99,69 @@ public class TodoActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //------this is for the detail button--------
+        Button buttonTodoDetail = (Button) findViewById(R.id.buttonTodoDetail);
+        buttonTodoDetail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Note, the child class being called has a static method determining the parameter
+                // to be passed to it in the intent object
+                Intent intent = TodoDetailActivity.newIntent(TodoActivity.this, mTodoIndex);
+
+                // second param requestCode identifies the call as there could be many "intents"
+                startActivityForResult(intent, IS_SUCCESS);
+                // The result will come back through
+                // onActivityResult(requestCode, resultCode, Intent) method
+
+            }
+        });
+
+    }
+
+    /*
+       requestCode is the integer request code originally supplied to startActivityForResult
+       resultCode is the integer result code returned by the child activity through its setResult()
+       intent date attached with intent "extras"
+   */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == IS_SUCCESS ){
+            if (intent != null) {
+                // data in intent from child activity
+                boolean isTodoComplete = intent.getBooleanExtra(IS_TODO_COMPLETE, false);
+                updateTodoComplete(isTodoComplete);
+            } else {
+                Toast.makeText(this, R.string.back_button_pressed, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, R.string.request_code_mismatch,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void updateTodoComplete(boolean is_todo_complete) {
+
+        final TextView textViewTodo;
+        textViewTodo = (TextView) findViewById(R.id.textViewTodo);
+
+        if (is_todo_complete) {
+            textViewTodo.setBackgroundColor(
+                    ContextCompat.getColor(this, R.color.backgroundSuccess));
+            textViewTodo.setTextColor(
+                    ContextCompat.getColor(this, R.color.colorSuccess));
+
+            setTextViewComplete("\u2713");
+        }
+
+    }
+
+    private void setTextViewComplete( String message ){
+        final TextView textViewComplete;
+        textViewComplete = (TextView) findViewById(R.id.textViewComplete);
+
+        textViewComplete.setText(message);
     }
 }
